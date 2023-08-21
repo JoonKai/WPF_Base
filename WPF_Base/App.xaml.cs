dae1499ba6fe3e6a6b01ed6e472639c1c68427e8
준석 손 <thstjr0487@naver.com>
2023-08-22 02:05:36 +0900

    초기 커밋.

---
+++ WPF_Base/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WPF_Base.Services;
+
+namespace WPF_Base
+{
+    /// <summary>
+    /// App.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly IServiceProvider _services = null;
+        public App()
+        {
+            _services = ConfigureService();
+        }
+
+        private IServiceProvider ConfigureService()
+        {
+            
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            
+        }
+    }
+}
---
+++ WPF_Base/Services/ViewService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WPF_Base.ViewModels;
+using WPF_Base.Views;
+
+namespace WPF_Base.Services
+{
+    public class ViewService : IViewService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public ViewService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+        public void ShowView<TView, TViewModel>(object parameter = null)
+            where TView : Window
+            where TViewModel : INotifyPropertyChanged
+        {
+            var view = (Window)_serviceProvider.GetService(typeof(TView));
+            var viewModel = (INotifyPropertyChanged)_serviceProvider.GetService(typeof(TViewModel));
+
+            //매개변수 역할 확인
+            if(parameter != null && viewModel is IParameterReceiver parameterReceiver)
+            {
+                parameterReceiver.ReceiveParameter(parameter);
+            }
+
+
+            view.DataContext = viewModel;
+            view.Show();
+        }
+        public void ShowMainView()
+        {
+            ShowView<MainView, MainViewModel>();
+        }
+        private bool ActivateView<TView>() where TView : Window
+        {
+            IEnumerable<Window> windows = Application.Current.Windows.OfType<TView>();
+
+            if (windows.Any())
+            {
+                windows.ElementAt(0).Activate();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void ShowSettingView()
+        {
+            if (!ActivateView<SettingView>())
+            {
+                ShowView<SettingView, SettingViewModel>();
+            }
+        }
+
+    }
+}
