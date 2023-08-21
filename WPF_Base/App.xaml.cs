2908a8f05c74a1607e9c8ac7708fa6b42de9608b
준석 손 <thstjr0487@naver.com>
2023-08-22 02:24:40 +0900

    1111

--- WPF_Base/App.xaml.cs
+++ WPF_Base/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -6,6 +7,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using WPF_Base.Services;
+using WPF_Base.ViewModels;
+using WPF_Base.Views;
 
 namespace WPF_Base
 {
@@ -22,11 +25,24 @@ namespace WPF_Base
 
         private IServiceProvider ConfigureService()
         {
-            
+            IServiceCollection services = new ServiceCollection();
+            //View
+            services.AddSingleton<MainView>();
+            services.AddTransient<SettingView>();
+            //ViewModel
+            services.AddSingleton<MainViewModel>();
+            services.AddTransient<SettingViewModel>();
+            //Service
+            services.AddSingleton<IViewService, ViewService>();
+
+
+            return services.BuildServiceProvider();
         }
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+            var viewService = (IViewService)_services.GetService(typeof(IViewService));
+            viewService.ShowMainView();
             
         }
     }
--- WPF_Base/Commands/RelayCommand.cs
+++ WPF_Base/Commands/RelayCommand.cs
@@ -8,18 +8,31 @@ using System.Windows.Input;
 
 namespace WPF_Base.Commands
 {
-    public class RelayCommand : ICommand
+    public class RelayCommand<T> : ICommand
     {
-        public event EventHandler CanExecuteChanged;
 
+        public event EventHandler CanExecuteChanged;
+        private readonly Action<T> _excute;
+        private readonly Predicate<T> _canExcute;
+
+        public RelayCommand(Action<T> excute, Predicate<T> canExcute = null)
+        {
+            this._excute = excute;
+            this._canExcute = canExcute;
+        }
+        public event EventHandler CanExcuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
         public bool CanExecute(object parameter)
         {
-            throw new NotImplementedException();
+            return _canExcute?.Invoke((T)parameter) ?? true;
         }
 
         public void Execute(object parameter)
         {
-            throw new NotImplementedException();
+            _excute?.Invoke((T)parameter);
         }
     }
 }
